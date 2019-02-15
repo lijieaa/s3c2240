80c88dae1f66f687d8a4afa4c2a621ab9ef1da30
jay <9520547672@qq.com>
2019-02-15 18:00:23 +0800

    init

--- led/led.c
+++ led/led.c
@@ -1,24 +1,25 @@
 
 
-void delay(volatile int d)
+void delay ( volatile int d )
 {
-	while (d--)
+	while ( d-- )
 		;
 }
 
-int main(int argc, char *argv[])
+int main ( int argc, char* argv[] )
 {
-	volatile unsigned int *pGPFCON = (volatile unsigned int *)0x56000050;
-	volatile unsigned int *pGPFDAT = (volatile unsigned int *)0x56000054;
+	volatile unsigned int* pGPFCON = ( volatile unsigned int* ) 0x56000050;
+	volatile unsigned int* pGPFDAT = ( volatile unsigned int* ) 0x56000054;
 
-	*pGPFCON &= ~((3 << 8) | (3 << 10) | (3 << 12));
-	*pGPFCON |= ((1 << 8) | (1 << 10) | (1 << 12));
+	*pGPFCON &= ~ ( ( 3 << 8 ) | ( 3 << 10 ) | ( 3 << 12 ) );
+	*pGPFCON |= ( ( 1 << 8 ) | ( 1 << 10 ) | ( 1 << 12 ) );
 
-	while (1)
+	while ( 1 )
 	{
-		*pGPFDAT &= ~(7 << 4);
-		delay(100000);
-		*pGPFDAT |= (7 << 4);
+		*pGPFDAT &= ~ ( 7 << 4 );
+		delay ( 100000 );
+		*pGPFDAT |= ( 7 << 4 );
 	};
+
 	return 0;
 }
